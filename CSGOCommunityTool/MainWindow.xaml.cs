a3964df091c5fd10b7cbc09f8bf5e1e3bdecb72c
MathijsBesten <m88_db@hotmail.com>
2016-05-27 09:20:19 +0200

    added (not complete) newsreader

--- CSGOCommunityTool/MainWindow.xaml.cs
+++ CSGOCommunityTool/MainWindow.xaml.cs
@@ -23,6 +23,41 @@ namespace CSGOCommunityTool
         public MainWindow()
         {
             InitializeComponent();
+            int count = 0;
+            Funtions.NewsReader.newsReader();
+            foreach (var item in Funtions.NewsReader.allItems)
+            {
+                foreach (var stringObject in item)
+                {
+                    if (count == 0)
+                    {
+                        newsSpot1.Text = stringObject;
+                    }
+                    else if (count == 1)
+                    {
+                        newsSpot2.Text = stringObject;
+                    }
+                    else if (count == 2)
+                    {
+                        newsSpot3.Text = stringObject;
+                    }
+                    else if (count == 3)
+                    {
+                        newsSpot4.Text = stringObject;
+                    }
+                    else if (count == 4)
+                    {
+                        newsSpot5.Text = stringObject;
+                    }
+                    else if (count == 5)
+                    {
+                        newsSpot6.Text = stringObject;
+                    }
+
+                }
+                count++;
+
+            }
         }
     }
 }
