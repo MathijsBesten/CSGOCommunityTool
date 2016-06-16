63fa9c48b71df2c83f6b4d0d0467968b8e4e4b43
RomainvanMaanen <ro.vanmaanen@gmail.com>
2016-06-16 12:10:18 +0200

    friend list

--- CSGOCommunityTool/Menu/ProfileCSGOStats.xaml.cs
+++ CSGOCommunityTool/Menu/ProfileCSGOStats.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CSGOCommunityTool.functions;
 
 namespace CSGOCommunityTool.Menu
 {
@@ -21,25 +22,43 @@ namespace CSGOCommunityTool.Menu
     public partial class ProfileCSGOStats : UserControl
     {
         public string playerFriendUrl = "http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=14A21E6B2EC8A4B857AA20CF416B38DE&steamid=";
+        public string playerDetailUrl = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=14A21E6B2EC8A4B857AA20CF416B38DE&steamids=";
         public static string steamAPIey = "14A21E6B2EC8A4B857AA20CF416B38DE";
         public static string UserId = "";
+        public static List<List<string>> friendlist = new List<List<string>>();
         public ProfileCSGOStats()
         {
-
             InitializeComponent();
-            //var UserId = CSGOCommunityTool.functions.ProfileSaver_ProfileReader.checkForProfile();
-            var UserId = "76561198009299347";
+            UserId = functions.ProfileSaver_ProfileReader.checkForProfile();
+            FriendList();
+            lb_friendnumber.Content = friendlist.Count().ToString();
+
+        }
+        public async void FriendList()
+        {
             if (UserId == "NoProfileFound")
             {
                 MessageBox.Show("You Need to log in first");
             }
             else
             {
+
                 var profileFriends = functions.JSONReader.ReadJSON(playerFriendUrl + UserId, public_properties.SteamProfile.playerStats, public_properties.SteamProfile.playerStatsEnd); // get all friends from profile
+                foreach (var friend in profileFriends)
+                {
+                    var friends = functions.JSONReader.ReadJSON(playerDetailUrl + friend, public_properties.SteamProfile.playerStats, public_properties.SteamProfile.playerStatsEnd); // gets all information about profile
+                    friendlist.Add(friends);
+                }
             }
+            lb_friendnumber.Content = friendlist.Count().ToString();
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
